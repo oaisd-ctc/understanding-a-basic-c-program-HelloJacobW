fd0b5067b4d908eb089d790b3e5a375bda554d84
HelloJacobW <jawoa4d@student.oaisd.org>
2024-10-07 17:42:38 +0000

    Did some code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Jacob Woudwyk, and my life...is kinda crazy.");
+        int a = 17;
+        string ln = "Woudwyk";
+        string g = "Hi!";
+        string fn = "Jake";
+        double m = 500.87;
+        Console.WriteLine("Copy of Copy of Copy of Copy of Copy of Copy of Copy of Copy of "+g+"! "+"My name is "+fn+" "+ln+".Age:"+a+" years. I have just spent "+m+"on an energy Drink.");
+    }
+}
